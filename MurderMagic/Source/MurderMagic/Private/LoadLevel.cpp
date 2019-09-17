57c9e6eed7a34d19bbf1d9384193d57735e73d24
Rebecca Stewart <44781452+beckrpgirl@users.noreply.github.com>
2019-09-17 15:01:07 -0400

    Added in Level features
    Added in a second map for a new level, along with created a BP where you can walk into and move into the next level.

---
+++ MurderMagic/Source/MurderMagic/Private/LoadLevel.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LoadLevel.h"
+#include "MurderMagicCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+
+ALoadLevel::ALoadLevel(const FObjectInitializer& OI)
+	: Super(OI)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionBox = OI.CreateDefaultSubobject<UBoxComponent>(this, TEXT("SphereComponent"));
+	CollisionBox->GetCollisionResponseToChannel(ECC_WorldStatic);
+	CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CollisionBox->SetGenerateOverlapEvents(true);
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ALoadLevel::OnOverlapBegin);
+	SetRootComponent(CollisionBox);
+
+	Mesh = OI.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("MeshComponent"));
+	Mesh->SetupAttachment(CollisionBox);
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+
+}
+
+// Called when the game starts or when spawned
+void ALoadLevel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALoadLevel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ALoadLevel::OnOverlapBegin(UPrimitiveComponent* OverlapComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Hit)
+{
+
+	UGameplayStatics::OpenLevel(this, LevelName, false);
+
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Purple, "Player Overlapped on load Level");
+
+
+}
+
