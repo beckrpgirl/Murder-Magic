275e091064ccfc51755c66c4f1bb5cefbb0f01d3
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-09-19 15:07:54 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/beckrpgirl/Murder-Magic""
    This reverts commit 1ecf51c4513ca1704889894740b59396caffd6ff.

---
+++ MurderMagic/Source/MurderMagic/Public/MainMenuLevel.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "MainMenuLevel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MURDERMAGIC_API AMainMenuLevel : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+		virtual void BeginPlay() override;
+
+
+};
