7640fb8bb1e2ca19c35ea692fb64ac3d1fe9fd9c
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2019-09-20 16:34:28 -0400

    Many AI updates
    Setting up the spawners and the AI manager.

---
+++ MurderMagic/Source/MurderMagic/Public/T2_Ogre.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NPC.h"
+#include "T2_Ogre.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MURDERMAGIC_API AT2_Ogre : public ANPC
+{
+	GENERATED_BODY()
+	
+};
