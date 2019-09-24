f3c96a65b3f810b755c2f79a171a29d45a3a8cff
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2019-09-24 13:51:52 -0400

    made slight changes to spells
    made slight changes to the spells regarding casts and particles, further work to be done

--- MurderMagic/Source/MurderMagic/Private/Spell.cpp
+++ MurderMagic/Source/MurderMagic/Private/Spell.cpp
@@ -9,6 +9,19 @@ ASpell::ASpell()
 	sinceCast = 0;
 	range = 0;
 	baseDMG = 0;
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	CollisionSphere->InitSphereRadius(40.0f);
+	CollisionSphere->GetCollisionResponseToChannel(ECC_WorldStatic);
+	CollisionSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CollisionSphere->SetGenerateOverlapEvents(true);
+	CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &ASpell::OnOverlapBegin);
+	SetRootComponent(CollisionSphere);
+
+	PSC = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleComponent"));
+	PSC->SetupAttachment(CollisionSphere);
+	PSC->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
 void ASpell::Cast(FVector start, float angle)
@@ -18,4 +31,8 @@ void ASpell::Cast(FVector start, float angle)
 	destination.Y = start.Y + (FMath::Sin(angle) * range);
 	destination.Z = start.Z;
 
+}
+
+void ASpell::OnOverlapBegin(UPrimitiveComponent* OverlapComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Hit)
+{
 }
