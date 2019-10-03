5b642e0be7269566fda3afa7603d865649071444
StevenCoverdale <S.coverdale@hotmail.com>
2019-10-03 16:57:00 -0400

    Edited Spawns
    Spawns now handle per UnitSpawnData (One Unit) - such as unit type with a min/max count of that type of unit.
    
    * Units are spawned at random locations provided by Targetpoints linked via the detail panels.
    
    * BP_Spawner now hides its mesh during gameplay, but is visible during development.
    
    * Once your BP_Spawner is deployed in the level - under the detail panel, we can see under the Spawner tab several bits of information such as UnitSpawnData, SpawnZones and SpawnMaxDistance.
    
    * UnitSpawnData, holds the information per wave such as unit type and min/max count of said unit.
    
    * SpawnZones, Each spawnZone is a location at which the Units can spawn at.
    
    * SpawnMaxDistance, The max distance a unit can spawn from its selected SpawnZone.

--- MurderMagic/Source/MurderMagic/Public/Enemies/Spawner.h
+++ MurderMagic/Source/MurderMagic/Public/Enemies/Spawner.h
@@ -9,6 +9,21 @@
 #include "Engine/EngineTypes.h"
 #include "Spawner.generated.h"
 
+class ATargetPoint;
+
+USTRUCT()
+struct FSpawnData
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = SpawnData)
+	TSubclassOf<class ANPC> unit;
+	UPROPERTY(EditAnywhere, Category = SpawnData)
+	int MinCount;
+	UPROPERTY(EditAnywhere, Category = SpawnData)
+	int MaxCount;
+};
+
 UCLASS()
 class MURDERMAGIC_API ASpawner : public AActor
 {
@@ -18,51 +33,21 @@ public:
 	// Sets default values for this actor's properties
 	ASpawner(const FObjectInitializer& OI);
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+private:
 	UPROPERTY(VisibleDefaultsOnly)
-		UBoxComponent* CollisionBox;
+	UBoxComponent* CollisionBox;
 
-	//three different classes can be set to spawn from one point.
-	UPROPERTY(EditAnywhere, Category = SpawnSettings)
-		TSubclassOf<class ANPC> ToSpawn_1;
-	UPROPERTY(EditAnywhere, Category = SpawnSettings)
-		TSubclassOf<class ANPC> ToSpawn_2;
-	UPROPERTY(EditAnywhere, Category = SpawnSettings)
-		TSubclassOf<class ANPC> ToSpawn_3;
+	UPROPERTY(EditAnywhere, Category = "Spawner|SpawnData")
+	TArray<FSpawnData> UnitSpawnData;
+
+	UPROPERTY(EditAnywhere, Category = "Spawner|SpawnZones")
+	TArray<ATargetPoint*> SpawnZones;
 	//Max and Min setting for number of spawns to be produced.
-	UPROPERTY(EditAnywhere, Category = SpawnSettings)
-		int Max;
-	UPROPERTY(EditAnywhere, Category = SpawnSettings)
-		int Min;
-	//TimeDelay in spawns.
-	UPROPERTY(EditAnywhere, Category = SpawnSettings)
-		float TimeDelay;
+	UPROPERTY(EditAnywhere, Category = "Spawner|SpawnZones")
+	int SpawnMaxDistance;
 
 	UFUNCTION()
-		virtual void OnOverlapBegin(UPrimitiveComponent* OverlapComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Hit);
-	UFUNCTION()
-		virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlapComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Hit);
 
-	float i;
-	int K;
-	void SpawnDelay();
-	void RandomNPC();
-
-	bool SpawnNow;
-	bool Used;
-	float XNPC;
-	int RandomNumber();
-	int RNum;
-
-	FVector Location;
-	FRotator Rotation;
-
-	FTimerHandle _TimerHandle;
+	void SpawnWave();
 };
