0209598ac2e67d2b7773bcd65f815d1dadcc0395
Jon Hunt <44199365+huntj1283@users.noreply.github.com>
2019-10-03 15:39:21 -0400

    Instruction Panel is done
    Instructions are finished with a button on the panel returning to the main menu

--- MurderMagic/Source/MurderMagic/Private/UI_InstructPanel.cpp
+++ MurderMagic/Source/MurderMagic/Private/UI_InstructPanel.cpp
@@ -2,4 +2,25 @@
 
 
 #include "UI_InstructPanel.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
 
+bool UUI_InstructPanel::Initialize()
+{
+	bool Success = Super::Initialize();
+	if (!Success) return false;
+1
+	if (InstructExitButton)
+	{
+		InstructExitButton->OnClicked.AddDynamic(this, &UUI_InstructPanel::OnClickInstructExitEvent);
+	}
+	return true;
+}
+
+void UUI_InstructPanel::OnClickInstructExitEvent()
+{
+	
+		this->SetVisibility(ESlateVisibility::Hidden);
+		
+		return;
+}
